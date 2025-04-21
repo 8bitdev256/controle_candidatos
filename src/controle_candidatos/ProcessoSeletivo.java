2d53ce1543261b884e1d84f4bd6bf758061ca2ee
8bitdev256 <8bitdev256@gmail.com>
2025-04-21 18:25:25 -0300

    FEAT: Fazendo ligações para cada candidato selecionado

--- src/controle_candidatos/ProcessoSeletivo.java
+++ src/controle_candidatos/ProcessoSeletivo.java
@@ -1,10 +1,45 @@
 package controle_candidatos;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		imprimirSelecionados();
+		String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+		for(String candidato : candidatos) {
+			entrandoEmContato(candidato);
+		}
+	}
+	
+	static void entrandoEmContato(String candidato) {
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu = false;
+		
+		do {
+			atendeu = atender();
+			continuarTentando = !atendeu;
+			
+			if (continuarTentando) {
+				tentativasRealizadas++;
+			}
+			else {
+				System.out.println("Contato realizado com sucesso");
+			}
+			
+		}
+		while(continuarTentando && tentativasRealizadas < 3);
+		
+		if (atendeu) {
+			System.out.println("Conseguimos contato com candidato " + candidato + " na tentativa " + tentativasRealizadas);
+		}
+		else {
+			System.out.println("Não conseguimos contato com candidato " + candidato + ". Número máximo de tentativas realizadas: " + tentativasRealizadas);
+		}
+	}
+	
+	static boolean atender() {
+		return new Random().nextInt(3) == 1;
 	}
 	
 	static void imprimirSelecionados() {
